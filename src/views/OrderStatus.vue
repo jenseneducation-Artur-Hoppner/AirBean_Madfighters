9c86d7c78e330073245fade3bdded2c32becd4e7
Lova Wins <lovaunger@gmail.com>
2020-03-20 13:33:05 +0100

    fixed the css

--- src/views/OrderStatus.vue
+++ src/views/OrderStatus.vue
@@ -1,12 +1,10 @@
 <template>
-  <main class="wrapper">
-
-    <p>Ordernummer #12DV23F</p>
-    <!-- <img src="graphics/drone.svg" alt="drone"> -->
+<main class="wrapper">
+    <p id="order">Ordernummer <span style="font-weight:700;">#12DV23F</span></p>
+    <img src="graphics/drone.svg" alt="drone">
     <h1>Din beställning är på väg!</h1>
     <p id="ETA">13 minuter</p>
-    <button>Ok, cool!</button>
-
+    <button class="btn">Ok, cool!</button>
 </main>
 </template>
 
@@ -17,17 +15,23 @@ export default {
 </script>
 
 <style>
+
 :root {
     background: #E5674E;
 }
 .wrapper {
     text-align: center;
+    padding:3%;
 }
 p {
-    font-family: 'fira sans';
+    font-family: montserrat, helvetica, sans-serif;
     font-size: 1em;
     margin: 16px 0;
     color: #fff;
+}
+
+#order {
+    font-family: montserrat, helvetica, sans-serif;
 }
 
 h1{
@@ -36,5 +40,14 @@ h1{
     line-height: 1.4em;
     color: #fff;
 } 
+
+.btn {
+    width: 10em;
+    height: 3em;
+    border-radius: 20px;
+    font-size: 1.5em;
+    font-family: 'Playfair Display';
+    font-weight: 700;
+}
 
 </style>
