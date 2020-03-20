70b5f0b66c8daf7b371c904030bdd463dcf3ca55
Lova Wins <lovaunger@gmail.com>
2020-03-20 13:21:20 +0100

    html done

--- src/views/OrderStatus.vue
+++ src/views/OrderStatus.vue
@@ -1,5 +1,13 @@
 <template>
-  
+  <main class="wrapper">
+
+    <p>Ordernummer #12DV23F</p>
+    <!-- <img src="graphics/drone.svg" alt="drone"> -->
+    <h1>Din beställning är på väg!</h1>
+    <p id="ETA">13 minuter</p>
+    <button>Ok, cool!</button>
+
+</main>
 </template>
 
 <script>
@@ -9,5 +17,24 @@ export default {
 </script>
 
 <style>
+:root {
+    background: #E5674E;
+}
+.wrapper {
+    text-align: center;
+}
+p {
+    font-family: 'fira sans';
+    font-size: 1em;
+    margin: 16px 0;
+    color: #fff;
+}
+
+h1{
+    font-family: 'playfair display';
+    font-size: 5rem;
+    line-height: 1.4em;
+    color: #fff;
+} 
 
 </style>
