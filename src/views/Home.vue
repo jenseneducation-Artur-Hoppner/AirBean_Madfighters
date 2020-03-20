9f0d51a94924eb85c83ceb40a1f86b974bc05e9d
Lova Wins <lovaunger@gmail.com>
2020-03-20 10:46:04 +0100

    add css and html

---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+ <div class="wrapper">
+<main class="main">
+    <img id="logo" src="@/assets/graphics/logo.svg">
+<h1>AIR BEAN</h1>
+<p>DRONEDELIVERED COFFEE</p>
+</main>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<style scoped>
+
+#logo{
+    width:6em;
+}
+
+.wrapper {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #38846d;
+    text-align: center;
+    color: #FCFEFD;
+    background-image: url("../assets/graphics/intro-graphic-right.svg"), url("../assets/graphics/intro-graphic-left.svg");
+    background-position: right, left;
+    background-repeat: no-repeat, no-repeat;
+} 
+
+/* Typography */
+h1,h2,h3,h4 {
+    font-family: 'playfair display';
+    font-size: 5rem;
+    line-height: 1.5em;
+} 
+
+p {
+    font-family: 'fira sans';
+    letter-spacing: .3em;
+    font-weight: 100;
+}
+
+</style>
