46ab014eb41ea7e43fca23fd569f60fbdea55a4f
Adetokunbo Adenuga <adetokunboadenuga@gmail.com>
2020-03-23 00:01:42 +0100

    My file starting point

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import VueRouter from 'vue-router';
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
