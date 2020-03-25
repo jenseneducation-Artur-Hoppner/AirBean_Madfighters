c268358d57bb141b16ed68b65d12e1553f3c4204
Adetokunbo Adenuga <adetokunboadenuga@gmail.com>
2020-03-25 11:51:52 +0100

    minor change

--- src/components/CartItems.vue
+++ src/components/CartItems.vue
@@ -1,15 +1,15 @@
 <template>
+    <div>
 
+    </div>
 </template>
 
 <script>
-
-export default {
-
-}
-
+    export default {
+        
+    }
 </script>
 
-<style lang='scss'>
+<style scoped>
 
 </style>
